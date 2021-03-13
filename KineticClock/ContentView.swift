a6e794998d86804089dfff35bd6be8bd44a6abaf
Waseem Akram <waseem07799@gmail.com>
2021-03-13 15:00:13 +0530

    Initial Commit

---
+++ KineticClock/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  KineticClock
+//
+//  Created by Waseem Akram on 13/03/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ KineticClock/KineticClockApp.swift
@@ -0,0 +1,17 @@
+//
+//  KineticClockApp.swift
+//  KineticClock
+//
+//  Created by Waseem Akram on 13/03/21.
+//
+
+import SwiftUI
+
+@main
+struct KineticClockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
